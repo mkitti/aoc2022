6c70eae0926ea1b8940f37fa61d09ae9bd204db3
Mark Kittisopikul <markkitt@gmail.com>
2022-12-09 01:03:23 -0500

    Add template

---
+++ template/src/template.jl
@@ -0,0 +1,17 @@
+module Day
+
+export read_input, part1, part2
+
+input_file() = "example.txt"
+# input_file() = "input.txt"
+
+function read_input(file = input_file())
+end
+
+function part1(input = read_input())
+end
+
+function part2(input = read_input())
+end
+
+end
